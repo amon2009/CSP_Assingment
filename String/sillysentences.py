9e518d97737f953850076e7662a6b860cf6010d7
amon2009 <amon.dipp@ucas-edu.net>
2024-09-20 19:03:09 +0000

    sillysentence.py is working

---
+++ String/sillysentences.py
@@ -0,0 +1,4 @@
+animal = input("Name a Animal: ")
+place = input("Name a Place: ")
+verb = input("Name a tense verb: ")
+print("the", animal, "walked to the", place,"and", verb,"with his friend angry turtle.")
