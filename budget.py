eb56779466959a4024f90a7b6c63f04c5062242c
amon2009 <amon.dipp@ucas-edu.net>
2024-09-20 19:29:23 +0000

    budget.py is ready

--- budget.py
+++ budget.py
@@ -11,5 +11,16 @@ print("Your monthly income is", income)
 print("Your expenses are", expenses)
 print("Your monthly savings is", savings)
 print("Your spending money is", spend)
-prent = (utilities/income)%100
-print(prent)
+prent = (rent/income) * 100
+pgroceries = (grocieries/ income)* 100
+psaving = (savings/ income) * 100
+pexpenses = (expenses/ income) * 100
+putilities= (utilities/ income) * 100
+pcar = (car/income) * 100
+print("Your rent is", prent,"% of your income." )
+print("Your utilitites is ", putilities,"% of your income")
+print("Your groceries is", pgroceries,"% of your income" )
+print("Your transport is", pcar,"% of your income")
+print("Your expenses is", pexpenses,"% of your income")
+print("Your savings is,", psaving," % of your income")
+
