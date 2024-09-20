26464c31050f914fe5f2d9ff42c1a0ab549df011
amon2009 <amon.dipp@ucas-edu.net>
2024-09-20 19:44:03 +0000

    expressions.c is working

---
+++ Expressions/expressions.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) { 
+  float first = (7-24/8*4+6);
+  printf("First =%f,\n", first );
+  float second = (18/3-7+2*5);
+  printf("Second =%f, \n", second);
+    return 0; 
+}
