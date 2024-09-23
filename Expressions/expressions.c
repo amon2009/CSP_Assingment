a7b4ae02ac1f09d99189f5478e432610a50f9e41
amon2009 <amon.dipp@ucas-edu.net>
2024-09-23 19:12:51 +0000

    expressions.c is ready

--- Expressions/expressions.c
+++ Expressions/expressions.c
@@ -3,7 +3,29 @@
 int main(void) { 
   float first = (7-24/8*4+6);
   printf("First =%f,\n", first );
+  
   float second = (18/3-7+2*5);
   printf("Second =%f, \n", second);
-    return 0; 
+  
+  float third = (6*4/12+72/8-9);
+  printf("Third =  %f \n", third);
+
+  float forth = ((17-6/2)+4*3);
+  printf("Forth = %f \n ", forth);
+
+  float fifth = (-2*(1*4-2/2)+(6+2-3));
+  printf("Fifth = %f \n"    , fifth);
+
+  float sixth = (-1*((3-4*7)/5)-2*24/6);
+  printf("Sixth = %f \n", sixth);
+
+  float seventh = ((3*(5*5)/15)-(5-(2*2)));
+  printf("Seventh = %f \n" , seventh);
+ 
+  float eighth = (((1*1*1*1)*(2*2)+(3*3*3)-(2*2*2*2*2)/4));
+  printf("Eighth = %f \n", eighth);
+
+  float ninth = ((22/2-2*5)*(22/2-2*5)+(4-6/6)*(4-6/6));
+  printf("Ninth = %f \n ", ninth);
+  return 0; 
 }
