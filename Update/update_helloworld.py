25f4069804fe4f04bd4aac36ba4ce5ae2d0e7586
amon2009 <amon.dipp@ucas-edu.net>
2024-09-24 19:22:38 +0000

    update helloworld is working

---
+++ Update/update_helloworld.py
@@ -0,0 +1,18 @@
+
+def hello(name):
+    
+    return f"Hello {name}."
+print(hello("Maria"))
+print(hello("John"))
+print(hello("Steve"))
+print(hello("Amon"))
+print(hello("Dean"))
+
+
+
+
+    
+
+
+
+
