4582b5f8ad38135cb53934f7ee2a92a8968c170c
amon2009 <amon.dipp@ucas-edu.net>
2024-10-01 01:07:24 +0000

    old.c is working

---
+++ Conditionals/old.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int age = 3;
+int main(void) { 
+    if (age >= 18){
+        printf("You are old enough to vote \n");
+    } else if (age >= 16){
+        printf("You are old enough to drive \n");
+    }else if (age == 15){
+        printf("You are old enough to get a learners permit \n");
+    }else if (age >= 5){
+        printf("You are old enough to go to school \n");
+    } else {
+        printf("Sorry, you are young enough to school");
+    };
+    return 0; 
+}
