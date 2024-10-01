1ca041100f554e376b0c2997b81d3fc582adffa3
amon2009 <amon.dipp@ucas-edu.net>
2024-10-01 00:22:58 +0000

    old.py is working

---
+++ Conditionals/old.py
@@ -0,0 +1,14 @@
+age = 20
+if age >= 18:
+    print("You are able to vote")
+elif age >= 16:
+    print("You are able to drive")
+elif age >= 15:
+    print("You are able to get a learners permit")
+elif age >= 5:
+    print("You are able to go to school")
+else:
+    print("Sorry, you are too young to school")
+
+
+
