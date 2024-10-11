1f9ae6682f01374dce305e1b7d4ac6ad2005063d
amon2009 <amon.dipp@ucas-edu.net>
2024-10-11 19:21:31 +0000

    fizzbuzz.py is working

--- Loops/fizzbuzz.py
+++ Loops/fizzbuzz.py
@@ -1,7 +1,13 @@
-for x in range(1,51,1):
-  print(x)
-if (x%3==0 and x%5==0):
-   print("FizzBuzz")
-else:
-   print("o")
+i = 1
+for x in range(1,51,i):
+ if (x%3==0 and x%5==0):
+   i = print("FizzBuzz")
+ elif (x%3==0):
+   i = print("Fizz")
+ elif (x%5==0):
+   i = print("Buzz")
+ else:
+   print(x)
+  
+ 
 
